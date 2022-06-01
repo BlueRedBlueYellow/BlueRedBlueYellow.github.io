8317906c362a07842ba2790207b911eb13506155
BlueRedBlueYellow <animalactiont05@googlemail.com>
2022-06-01 00:37:30 -0500

    Zoroark Spin Added, alt text on homepage fixed

--- README.md
+++
@@ -1,2 +0,0 @@
-# BlueRedBlueYellow.github.io
-My personal website!
--- navigationbar.js
+++ navigationbar.js
@@ -59,9 +59,13 @@ const overlay = document.getElementById("overlay");
 for (let button of desktop_nav_buttons) {
     button.addEventListener("mouseenter", () => {
         overlay.style.opacity = "0.8";
+        document.getElementById("page-content").style.zIndex = "-1";
     })
 
     button.addEventListener("mouseleave", () => {
         overlay.style.opacity = "0";
+        setTimeout(() => {
+            document.getElementById("page-content").style.zIndex = "0";
+        }, 200)
     })
 };
